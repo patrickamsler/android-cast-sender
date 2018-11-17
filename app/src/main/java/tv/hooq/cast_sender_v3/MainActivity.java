092bf1acf9e8c73fbb5160924dc7b23b3f54c2ec
paam <patrick.amsler@zuhlke.com>
2018-11-17 23:09:19 +0800

    imports organized

--- app/src/main/java/tv/hooq/cast_sender_v3/MainActivity.java
+++ app/src/main/java/tv/hooq/cast_sender_v3/MainActivity.java
@@ -1,16 +1,13 @@
 package tv.hooq.cast_sender_v3;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.gms.cast.MediaInfo;
 import com.google.android.gms.cast.MediaMetadata;
@@ -19,7 +16,6 @@ import com.google.android.gms.cast.framework.CastContext;
 import com.google.android.gms.cast.framework.CastSession;
 import com.google.android.gms.cast.framework.SessionManagerListener;
 import com.google.android.gms.cast.framework.media.RemoteMediaClient;
-import com.google.android.gms.common.images.WebImage;
 
 public class MainActivity extends AppCompatActivity {
 
