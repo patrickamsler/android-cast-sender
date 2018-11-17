07607773e15719b1e518f652e52e38c72bedd3dc
paam <patrick.amsler@zuhlke.com>
2018-11-17 23:08:53 +0800

    initial commit

---
+++ app/src/main/java/tv/hooq/cast_sender_v3/CastOptionsProvider.java
@@ -0,0 +1,24 @@
+package tv.hooq.cast_sender_v3;
+
+import com.google.android.gms.cast.framework.CastOptions;
+import com.google.android.gms.cast.framework.OptionsProvider;
+import com.google.android.gms.cast.framework.SessionProvider;
+
+import android.content.Context;
+
+import java.util.List;
+
+public class CastOptionsProvider implements OptionsProvider {
+
+    @Override
+    public CastOptions getCastOptions(Context context) {
+        return new CastOptions.Builder()
+                .setReceiverApplicationId(context.getString(R.string.app_id))
+                .build();
+    }
+
+    @Override
+    public List<SessionProvider> getAdditionalSessionProviders(Context context) {
+        return null;
+    }
+}
---
+++ app/src/main/java/tv/hooq/cast_sender_v3/MainActivity.java
@@ -0,0 +1,192 @@
+package tv.hooq.cast_sender_v3;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.cast.MediaInfo;
+import com.google.android.gms.cast.MediaMetadata;
+import com.google.android.gms.cast.framework.CastButtonFactory;
+import com.google.android.gms.cast.framework.CastContext;
+import com.google.android.gms.cast.framework.CastSession;
+import com.google.android.gms.cast.framework.SessionManagerListener;
+import com.google.android.gms.cast.framework.media.RemoteMediaClient;
+import com.google.android.gms.common.images.WebImage;
+
+public class MainActivity extends AppCompatActivity {
+
+    private enum CastSessionState {CONNECTED, DISCONNECTED}
+
+    private CastContext castContext;
+    private MenuItem mediaRouteMenuItem;
+    private Toolbar toolbar;
+    private CastSession castSession;
+    private SessionManagerListener<CastSession> sessionManagerListener;
+
+    private CastSessionState castSessionState = CastSessionState.DISCONNECTED;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (castSessionState == CastSessionState.CONNECTED) {
+                    loadRemoteMedia();
+                    Snackbar.make(view, "Remote media loaded...", Snackbar.LENGTH_SHORT)
+                            .setAction("Action", null).show();
+                }
+                else {
+                    Snackbar.make(view, "Not connected to chrome cast", Snackbar.LENGTH_SHORT)
+                            .setAction("Action", null).show();
+                }
+            }
+        });
+
+
+        setupActionBar();
+
+        castContext = CastContext.getSharedInstance(this);
+        setupCastListener();
+    }
+
+    private void setupActionBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        mediaRouteMenuItem = CastButtonFactory.setUpMediaRouteButton(getApplicationContext(), menu, R.id.media_route_menu_item);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        castContext.getSessionManager().removeSessionManagerListener(
+                sessionManagerListener, CastSession.class);
+    }
+
+    @Override
+    protected void onResume() {
+        castContext.getSessionManager().addSessionManagerListener(
+                sessionManagerListener, CastSession.class);
+        super.onResume();
+    }
+
+    private void loadRemoteMedia() {
+        if (castSession == null) {
+            return;
+        }
+        RemoteMediaClient remoteMediaClient = castSession.getRemoteMediaClient();
+        if (remoteMediaClient == null) {
+            return;
+        }
+
+        remoteMediaClient.load(buildMediaInfo(), true, 0);
+    }
+
+    private MediaInfo buildMediaInfo() {
+        MediaMetadata movieMetadata = new MediaMetadata(MediaMetadata.MEDIA_TYPE_MOVIE);
+
+        movieMetadata.putString(MediaMetadata.KEY_TITLE, "HOOQ Test Video");
+//        movieMetadata.addImage(new WebImage(Uri.parse(mSelectedMedia.getImage(0))));
+//        movieMetadata.addImage(new WebImage(Uri.parse(mSelectedMedia.getImage(1))));
+
+        return new MediaInfo.Builder("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4")
+                .setStreamType(MediaInfo.STREAM_TYPE_BUFFERED)
+                .setContentType("videos/mp4")
+                .setMetadata(movieMetadata)
+//                .setStreamDuration(mSelectedMedia.getDuration() * 1000)
+                .build();
+    }
+
+    private void setupCastListener() {
+        sessionManagerListener = new SessionManagerListener<CastSession>() {
+
+            @Override
+            public void onSessionEnded(CastSession session, int error) {
+                onApplicationDisconnected();
+            }
+
+            @Override
+            public void onSessionResumed(CastSession session, boolean wasSuspended) {
+                onApplicationConnected(session);
+            }
+
+            @Override
+            public void onSessionResumeFailed(CastSession session, int error) {
+                onApplicationDisconnected();
+            }
+
+            @Override
+            public void onSessionStarted(CastSession session, String sessionId) {
+                onApplicationConnected(session);
+            }
+
+            @Override
+            public void onSessionStartFailed(CastSession session, int error) {
+                onApplicationDisconnected();
+            }
+
+            @Override
+            public void onSessionStarting(CastSession session) {
+            }
+
+            @Override
+            public void onSessionEnding(CastSession session) {
+            }
+
+            @Override
+            public void onSessionResuming(CastSession session, String sessionId) {
+            }
+
+            @Override
+            public void onSessionSuspended(CastSession session, int reason) {
+            }
+
+            private void onApplicationConnected(CastSession castSession) {
+                MainActivity.this.castSession = castSession;
+                castSessionState = CastSessionState.CONNECTED;
+                supportInvalidateOptionsMenu();
+            }
+
+            private void onApplicationDisconnected() {
+                castSessionState = CastSessionState.DISCONNECTED;
+                supportInvalidateOptionsMenu();
+            }
+        };
+    }
+}
